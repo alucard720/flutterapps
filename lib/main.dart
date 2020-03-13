89dcd24ea43290a9abcc37817f95e1cefcc6a066
codex-linux <https://github.com/alucard720/flutterapps.git>
2020-03-13 09:49:26 -0400

    emp01 first strike

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width:100.0,
+                color: Colors.white,
+                child: Text('Block1')
+              ),
+              Container(
+                  height: 100.0,
+                  width:100.0,
+                  color: Colors.white,
+                  child: Text('Block1')
+              ),
+            ],
+          ),
+        ),
+        ),
+
+    );
+
+  }
+}
